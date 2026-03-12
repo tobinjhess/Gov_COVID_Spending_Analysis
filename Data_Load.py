a65fb13d01b5a0271ef169c131adde5f7136b9c1
tobinjhess <tobinjhess@gmail.com>
2026-03-12 15:48:03 -0400

    Refactor file paths to relative names
    Updated file paths to use relative names instead of absolute paths.

--- Data_Load.py
+++ Data_Load.py
@@ -8,13 +8,13 @@ Created on Wed Feb 25 16:21:23 2026
 
 import pandas as pd
 
-files = ['/Users/tobyh1/Gov_COVID_Spending/Data/PrimeAwardSummariesAndSubawards_2026-02-25_H18M37S35139579_2020/Contracts_PrimeAwardSummaries_2026-02-25_H18M42S42_1.csv',
-         '/Users/tobyh1/Gov_COVID_Spending/Data/PrimeAwardSummariesAndSubawards_2026-02-25_H19M01S18654835_2021/Contracts_PrimeAwardSummaries_2026-02-25_H19M01S23_1.csv',
-         '/Users/tobyh1/Gov_COVID_Spending/Data/PrimeAwardSummariesAndSubawards_2026-02-25_H19M03S08102334_2022/Contracts_PrimeAwardSummaries_2026-02-25_H19M03S11_1.csv',
-         '/Users/tobyh1/Gov_COVID_Spending/Data/PrimeAwardSummariesAndSubawards_2026-02-25_H19M04S39241208_2023/Contracts_PrimeAwardSummaries_2026-02-25_H19M04S41_1.csv',
-         '/Users/tobyh1/Gov_COVID_Spending/Data/PrimeAwardSummariesAndSubawards_2026-02-25_H19M07S56738868_2024/Contracts_PrimeAwardSummaries_2026-02-25_H19M07S58_1.csv',
-         '/Users/tobyh1/Gov_COVID_Spending/Data/PrimeAwardSummariesAndSubawards_2026-02-25_H19M11S22032779_2025/Contracts_PrimeAwardSummaries_2026-02-25_H19M11S23_1.csv',
-         '/Users/tobyh1/Gov_COVID_Spending/Data/PrimeAwardSummariesAndSubawards_2026-02-25_H19M14S30293461_2026/Contracts_PrimeAwardSummaries_2026-02-25_H19M21S10_1.csv',
+files = ['Contracts_PrimeAwardSummaries_2026-02-25_H18M42S42_1.csv',
+         'Contracts_PrimeAwardSummaries_2026-02-25_H19M01S23_1.csv',
+         'Contracts_PrimeAwardSummaries_2026-02-25_H19M03S11_1.csv',
+         'Contracts_PrimeAwardSummaries_2026-02-25_H19M04S41_1.csv',
+         'Contracts_PrimeAwardSummaries_2026-02-25_H19M07S58_1.csv',
+         'Contracts_PrimeAwardSummaries_2026-02-25_H19M11S23_1.csv',
+         'Contracts_PrimeAwardSummaries_2026-02-25_H19M21S10_1.csv',
     ]
 
 dfs = [pd.read_csv(file) for file in files]
